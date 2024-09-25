5e231f04b570129dae57f861d7dda177df7ddf25
joksanhaniel <joksanhaniel@gmail.com>
2024-09-25 11:42:44 -0600

    version desconocida

--- FalconIA/IA/models.py
+++ FalconIA/IA/models.py
@@ -18,7 +18,7 @@ class Document(models.Model):
     uploaded_at = models.DateTimeField(auto_now_add=True)
     product_specs = models.JSONField(null=True, blank=True)  # Cambiado de schneider_specs a product_specs
     brand = models.CharField(max_length=100, blank=True)  # Nuevo campo para la marca
-    
+
     def __str__(self):
         return self.file.name
 
